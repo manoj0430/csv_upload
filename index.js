716f4a94d817040598d0ec9d509f43f10bfc9bee
manasa lingabathula <manojl0430@gmail.com>
2024-02-17 19:54:53 +0530

    Change port to dynamic

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config();
-const port=process.env.CSV_PORT;
+const port=process.env.CSV_PORT || 8000;
 const app=express();
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
