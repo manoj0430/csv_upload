dfdeb74b0c11e4b1b7d7389a4262ab2227b72da6
manasa lingabathula <manojl0430@gmail.com>
2024-02-18 11:09:44 +0530

    Minor changes

--- app.js
+++ app.js
@@ -1,38 +1,35 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 
-const port=process.env.PORT || 8000;
-const app=express();
-const path = require('path');
-const expressLayouts = require('express-ejs-layouts');
-const routes = require('./routes');
-const db=require('./config/mongoose');
+const port = process.env.PORT || 8000;
+const app = express();
+const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
+const routes = require("./routes");
+const db = require("./config/mongoose");
 
 app.use(express.urlencoded());
 app.use(expressLayouts);
-app.use('/',routes);
+app.use("/", routes);
 // setting up the view engine as ejs
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 // setting up the views folder as ./views
-app.set('views',path.join(__dirname,'./views'));
+app.set("views", path.join(__dirname, "./views"));
 // way to declare upload file path make the upload path  to the browser
-app.use('/uploads',express.static(__dirname +'/uploads'));
-
-
-
-
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 // setting up the assests path
-app.use(express.static('./assets'));
+app.use(express.static("./assets"));
 
 // extract style and scripts from sub pages into the payout
-app.set('layout extractStyles',true);
-app.set('layout extractScripts',true);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
+app.listen(port, function (err) {
+  if (err) {
+    console.log("error in running the server", err);
+    return;
+  }
 
-app.listen(port,function(err){
-    if(err){console.log('error in running the server',err);
-    return;}
-    
-    console.log('Server is running on port :',port);
-})
+  console.log("Server is running on port :", port);
+});
